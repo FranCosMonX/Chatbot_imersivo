35684a402d4ce56e2c4fd248abc39a8ed1fb3d5d
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-05-10 21:03:54 -0300

    inserindo marcações de lista e de codigo

--- src/utils/sintaxe/texto.tsx
+++ src/utils/sintaxe/texto.tsx
@@ -4,10 +4,15 @@ interface sintaxeParams {
 }
 
 const aprimorando_sintaxe = ({ text }: sintaxeParams) => {
-  const textComNegrito = text.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>')
+  const textComNegrito = text.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
+
+  const textComLista = textComNegrito.replace(/[*|-] (.*?)(\n|$)/g, '<ul><li>$1</li></ul>');
+
+  const textComCodigo = textComLista.replace(/```([^`]*)```/g, '<pre><code>$1</code></pre>');
+
   return (
-    <div dangerouslySetInnerHTML={{ __html: textComNegrito }} />
-  )
+    <div dangerouslySetInnerHTML={{ __html: textComCodigo }} />
+  );
 }
 
 export default aprimorando_sintaxe
